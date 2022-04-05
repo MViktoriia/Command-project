fef088123955257fa8d9485112c3fb9c3e04761d
MViktoriia <mihaleva.victoria@gmail.com>
2022-04-06 01:11:56 +0300

    header start

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import './sass/main.scss';
+
+const fetchFilms = async () => {
+  const response = await fetch("https://api.themoviedb.org/3/movie/550?api_key=e3f9ac8b4c25ea958657e460a35663f5");
+  const films = await response.json();
+  return films;
+};
+
+fetchFilms()
+  .then(films => console.log(films))
+  .catch(error => console.log(error));
---
+++ src/js/apiSettings.js
@@ -0,0 +1,7 @@
+const settings = {
+    filmyApiKey: "e3f9ac8b4c25ea958657e460a35663f5",
+    FILM_API_URL: 'https://api.themoviedb.org/3/movie/550?api_key=e3f9ac8b4c25ea958657e460a35663f5',
+};
+
+export default settings;
+
